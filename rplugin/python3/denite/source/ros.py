896930525ca3138e5db83ba3717b170d6612cd78
Felix <felix@fzeltner.de>
2019-10-26 17:16:55 +0200

    allow specifying packages as input

--- rplugin/python3/denite/source/ros.py
+++ rplugin/python3/denite/source/ros.py
@@ -13,4 +13,5 @@ class Source(Base):
 
     def gather_candidates(self, context: UserContext) -> Candidates:
         rosp = rospkg.RosPack()
-        return [{'word': pkg, 'ros_pkg_path': rosp.get_path(pkg)} for pkg in rosp.list()]
+        pkg_list = context['args'] if context['args'] else rosp.list()
+        return [{'word': pkg, 'ros_pkg_path': rosp.get_path(pkg)} for pkg in pkg_list]
